5d935715e71432368e04db98d55a1b44c68cd3ca
amithkukllod777 <amithkukllod@gmail.com>
2026-07-09 17:29:55 +0530

    Add Capacitor Android config

---
+++ capacitor.config.ts
@@ -0,0 +1,19 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.kuklabs.kukpdf',
+  appName: 'KukPDF',
+  webDir: 'dist',
+  bundledWebRuntime: false,
+  plugins: {
+    Camera: {
+      permissions: ['camera', 'photos']
+    }
+  },
+  android: {
+    allowMixedContent: false,
+    captureInput: true
+  }
+};
+
+export default config;
